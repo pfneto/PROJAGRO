3a45819bf1fba1191c8cb73883715824206c318b
pfneto <pfneto@hotmail.com>
2017-09-18 01:50:13 -0300

    Signed-off-by: pfneto <pfneto@hotmail.com>

--- agroesb/src/main/java/com/microsoft/sqlserver/jdbc/SQLServerDriver.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.sqlserver.jdbc;
-
-import org.springframework.jdbc.core.metadata.SqlServerCallMetaDataProvider;
-
-public class SQLServerDriver extends SqlServerCallMetaDataProvider {
-
-}
